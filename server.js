dc96c64f62c2b3a28375dc378771ff738e81f0f9
Martin44J <Budbud04@yahoo.com>
2022-02-23 21:45:23 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const bodyParser = require("body-parser");
+
+var command = "";
+
+app.get("/", function(req, res){
+  res.send({command: command})
+});
+
+app.post("/", function(req,res){
+  command = req.query.command;
+  res.status(200).json({success: "true", command: command})
+});
+
+
+
+app.listen(3000, function(){
+  console.log("Starting server on Port 3000");
+});
